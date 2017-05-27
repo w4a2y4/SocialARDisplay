67bbc558f4b93e4a9546ca494ce2c2489dd46194
w4a2y4 <b04902079@ntu.edu.tw>
2017-05-27 21:58:17 +0800

    Add timer

--- list/fade.js
+++ list/fade.js
@@ -1,9 +1,12 @@
 var curr = 1;
+var timer;
 
 $(document).keyup(function (e){
 
-	// press enter : the list fade IN
+	// press enter : the list fade IN 
+	// (only work when there's nothing on the screen)
     if( e.which === 13 ) { 
+
     	if( curr > 7 ) {
 	    	for (var i = 1; i <= 7; i++) {
 	    		var w = ".word[name=w" + i + "]"; 
@@ -12,14 +15,24 @@ $(document).keyup(function (e){
 	    	curr = 1;
 	    }
     	else $('#l1').fadeIn(1000);
+
+    	timer = setTimeout( 'fadeWord()', 3000 );
+
     }
 
     // press space : a word fade OUT
     else if( e.which === 32 ) {
-    	if( curr > 7 ) return;
-    	var w = ".word[name=w" + curr + "]"; 
-    	$(w).fadeTo(1000, 0);
-    	curr += 1;
+    	fadeWord();
     }
 
 });
+
+function fadeWord() {
+	clearTimeout(timer);
+	if( curr > 7 ) return;
+	var w = ".word[name=w" + curr + "]"; 
+	$(w).fadeTo(1000, 0);
+	curr += 1;
+	timer = setTimeout( 'fadeWord()', 3000 );
+	return;
+}
