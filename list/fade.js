30c57b79a85b9d8a2ec45c2e984e894bc37c6e91
w4a2y4 <b04902079@ntu.edu.tw>
2017-05-27 21:23:44 +0800

    Fix the auto-filling-in problem

--- list/fade.js
+++ list/fade.js
@@ -5,21 +5,20 @@ $(document).keyup(function (e){
 	// press enter : the list fade IN
     if( e.which === 13 ) { 
     	if( curr > 7 ) {
-    		$('#l1').hide();
 	    	for (var i = 1; i <= 7; i++) {
 	    		var w = ".word[name=w" + i + "]"; 
-	    		$(w).show();
+	    		$(w).fadeTo(1000, 1);
 	    	}
+	    	curr = 1;
 	    }
-    	curr = 1;
-    	$('#l1').fadeIn(1000);
+    	else $('#l1').fadeIn(1000);
     }
 
     // press space : a word fade OUT
     else if( e.which === 32 ) {
     	if( curr > 7 ) return;
     	var w = ".word[name=w" + curr + "]"; 
-    	$(w).fadeOut(1000);
+    	$(w).fadeTo(1000, 0);
     	curr += 1;
     }
 
